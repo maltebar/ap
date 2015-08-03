037b0ac1f96ddf016475f663452cff969e1e8715
kenharsch <kharsch@brandeis.edu>
2015-08-03 11:09:16 -0400

    got wiki working with gollum, going to re-do with paperclip

--- ap/config/routes.rb
+++ ap/config/routes.rb
@@ -1,17 +1,8 @@
 require 'gollum/app'
-require 'gollum-lib'
+
 
 
 Rails.application.routes.draw do
-
-
-  Precious::App.set(:gollum_path, Rails.root.join('wiki').to_s)
-  Precious::App.set(:default_markup, :markdown) # set your favorite markup language
-  Precious::App.set(:wiki_options, {:universal_toc => true})
-  Precious::App.set(:wiki_options, {:css => true})
-  mount Precious::App, at: 'wiki'
-
-  get 'wiki/edit'
 
   devise_for :users
   resources :users
