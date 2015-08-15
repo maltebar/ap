b292a4b722118005364e2f3017c451cd7d092875
Maria Altebarmakian <m.altebarmakian@gmail.com>
2015-08-15 12:22:12 -0700

    projects form completed

--- ap/app/models/project.rb
+++ ap/app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
-	has_many :partnerships, through: :ownership
+	has_many :ownerships
+	has_many :partnerships, :through => :ownerships
 	has_many :design_cases
 	has_many :board_posts
 end
