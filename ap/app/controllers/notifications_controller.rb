893e4e4ee564bb6e84080929dc0b5d564c4099d6
kenharsch <kharsch@brandeis.edu>
2015-08-17 22:06:17 -0400

    added notifications to home page with create note box for admin. Put row definition in partial to separate each note

--- ap/app/controllers/notifications_controller.rb
+++ ap/app/controllers/notifications_controller.rb
@@ -69,6 +69,6 @@ class NotificationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def notification_params
-      params[:notification]
+      params.require(:notification).permit(:message, :link, :read)
     end
 end
