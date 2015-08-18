4aaddb3347fafe3c0f2c7ce37e9f6e2a7b0b6e52
kenharsch <kharsch@brandeis.edu>
2015-08-18 13:39:30 -0400

    notifications created for comments on post, show up in users notification feed with message.

--- ap/app/models/comment.rb
+++ ap/app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :board_post
+
+	after_save :notify
+
+	def notify
+		Notification.create(message: self.user.nickname.to_s + ' commented on your post ' + self.board_post.title.to_s, user_notified: self.board_post.user_id)
+	end
 end
