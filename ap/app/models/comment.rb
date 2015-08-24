640af70e24cf30fe82b70fd109569c96b0f65b9f
kenharsch <kharsch@brandeis.edu>
2015-08-24 17:58:03 -0400

    fixed notify for like comment

--- ap/app/models/comment.rb
+++ ap/app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :board_post
 
-	after_save :notify
+	after_create :notify
 
 	def notify
 		Notification.create(message: self.user.nickname.to_s + ' commented on your post ' + self.board_post.title.to_s, user_notified: self.board_post.user_id, link: '/'+'board_posts/'+self.board_post.id.to_s)
--- ap/app/models/comment_upvote.rb
+++ ap/app/models/comment_upvote.rb
@@ -1,5 +1,6 @@
 class CommentUpvote < ActiveRecord::Base
 	after_save :increment_upvotes
+	after_save :notify
 
 	belongs_to :user
 	belongs_to :comment
@@ -8,5 +9,9 @@ class CommentUpvote < ActiveRecord::Base
 		@comment = self.comment
 		current_upvotes = @comment.upvotes
 		@comment.update(upvotes: current_upvotes + 1)
+	end
+
+	def notify
+		Notification.create(message: self.user.nickname.to_s + ' liked your comment.', user_notified: self.comment.user_id, link: '/'+'board_posts/'+self.comment.board_post.id.to_s)
 	end
 end
