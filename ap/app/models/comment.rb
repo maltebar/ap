5ea96bd9e79ed3b70c62b47ca518450814bde0f3
kenharsch <kharsch@brandeis.edu>
2015-08-26 09:44:17 -0400

    comment on comment notifications working. also stopped notification for own comment on post

--- ap/app/models/comment.rb
+++ ap/app/models/comment.rb
@@ -5,6 +5,21 @@ class Comment < ActiveRecord::Base
 	after_create :notify
 
 	def notify
-		Notification.create(message: self.user.nickname.to_s + ' commented on your post ' + self.board_post.title.to_s, user_notified: self.board_post.user_id, link: '/'+'board_posts/'+self.board_post.id.to_s)
+		unless self.user_id == self.board_post.user_id
+			Notification.create(message: self.user.nickname.to_s + ' commented on your post ' + self.board_post.title.to_s, user_notified: self.board_post.user_id, link: '/'+'board_posts/'+self.board_post.id.to_s)
+		end
+		@post = self.board_post
+		commenters = Array.new
+		commenters << @post.user_id
+		commenters << self.user_id
+		@post.comments.each do |c|
+				if !commenters.include?(c.user_id)
+					Notification.create(message: self.user.nickname.to_s + ' also commented on ' + self.board_post.title.to_s, user_notified: c.user_id, link: '/'+'board_posts/'+self.board_post.id.to_s)
+					commenters << c.user_id
+				end
+		end
 	end
 end
+
+
+
